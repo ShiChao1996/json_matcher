afda0a7eb6c4801bf03419c0f2a73448ba8969e5
ShiChao1996 <2483061998@qq.com>
2021-01-11 21:30:28 +0800

    change setup.py; add gitignore

--- setup.py
+++ setup.py
@@ -6,18 +6,17 @@
 
 import setuptools
 
+with open("./README.md", "r") as f:
+    long_desc = f.read()
 setuptools.setup(
-    name="json_matcher-shichao1996",
+    name="json_matcher",
     version="0.0.1",
     author="Shichao1996",
     author_email="2483061998@qq.com",
     description="A simple json struct matcher",
-    long_description="A json struct matcher, allow you define a template which is expressed in json,"
-                     "and check if a json data matches the rule defined by the template. For more, "
-                     "it apply a way to get specific data in the template, "
-                     "and a way to find if part of a json data matches the template.",
-    long_description_content_type="text",
-    url="https://github.com/Shichao1996/json_matcher.github.io",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Shichao1996",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
