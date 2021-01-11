e291bff1f2c18726aaf5c85ddf9fec85ed2dbf8f
ShiChao1996 <2483061998@qq.com>
2021-01-11 23:39:21 +0800

    adjust project struct

--- json_matcher/__init__.py
+++ json_matcher/__init__.py
@@ -1,10 +1,10 @@
 # coding:utf-8
 """
-@time: 2021/1/9 5:23 下午
+@time: 2021/1/11 9:57 下午
 @author: shichao
 """
 
 from json_matcher.matcher import JsonMatcher
-from json_matcher.rule import or_, and_, get_, any_
+from json_matcher.rule import or_, and_, any_, get_
 
 __all__ = ['JsonMatcher', 'or_', 'and_', 'get_', 'any_']
--- json_matcher/matcher.py
+++ json_matcher/matcher.py
@@ -5,7 +5,7 @@
 """
 import json
 
-from rule import gen_rule
+from json_matcher.rule import gen_rule
 
 
 class JsonMatcher(object):
